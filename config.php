2073eecf4df1847795c92a11f17b817b192b1ad3
muhammadnopran <noprantor@gmail.com>
2018-09-06 08:23:25 +0700

    versi 1

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+/** 
+ * using mysqli_connect for database connection 
+ */ 
+  
+$databaseHost = 'localhost'; 
+$databaseName = 'crud_db'; 
+$databaseUsername = 'root'; 
+$databasePassword = ''; 
+  
+$mysqli = mysqli_connect($databaseHost, 
+	$databaseUsername, $databasePassword, 
+	$databaseName);  
+  
+?> 
